16254cd214239c4a38c4edade449199b93243dc2
Mehul Maggo <81161901+ApolloNow@users.noreply.github.com>
2022-11-12 10:03:22 +0530

    Add files via upload

---
+++ Assignment_5/Q1.java
@@ -0,0 +1,20 @@
+class Mammals{
+    void myFunc(){
+        System.out.println();
+    }
+}
+
+class MarineAnimals{
+
+}
+
+class BlueWhale{
+
+}
+
+public class Q1 {
+    public static void main(String[] args) {
+        
+    }
+    
+}

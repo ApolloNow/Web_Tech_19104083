c22ee935473598682d27b414ddf5b6012fb87636
Mehul Maggo <81161901+ApolloNow@users.noreply.github.com>
2022-11-28 17:52:57 +0530

    Update Q2.java

--- Assignment_6/Q2.java
+++ Assignment_6/Q2.java
@@ -1,6 +1,45 @@
+import java.util.*;
+
 public class Q2 {
-    public static void main(String[] args) {
-        
+    public static void addition(int num1, int num2) {
+        if(num1< 0|| num2 < 0) {
+            throw new ArithmeticException("Negative numbers Exception. Please enter positive numbers only");
+        }
+        else {
+            System.out.println(num1+num2);
+        }
     }
-    
+    public static void subtraction(int num1, int num2) {
+        if(num1 < 0 || num2< 0) {
+            throw new ArithmeticException("Negative numbers Exception. Please enter positive numbers only");
+        }
+        else {
+            System.out.println(num1-num2);
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Input the second number : ");
+        int num2 = sc.nextInt();
+        sc.close();
+
+        try {
+            addition(num1, num2);
+        }
+        catch(ArithmeticException e) { 
+            System.out.println(e);
+        }
+
+
+        try {
+            subtraction(num1, num2);
+        }
+        catch(ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
 }
